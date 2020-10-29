5122d15334453fc095099fe8bf629c763f395aa2
Vitali Metelev <devita.work@yandex.ru>
2020-10-29 22:07:25 +0400

    Починил текст

---
+++ Csharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите колличество дней:");
+            int day = Convert.ToInt32(Console.ReadLine());
+            int year = day / 365;
+            int month = (day % 365) / 30;
+            day = (day % 365) % 30;
+            Console.WriteLine($"Летт = {year} \nМесяцев = {month} \nДней = {day}");
+            Console.ReadKey();
+        }
+    }
+}

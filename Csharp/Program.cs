05b2cda70968b35a0dbe70d815621b7952ef5087
Vitali Metelev <devita.work@yandex.ru>
2020-10-30 13:13:41 +0400

    Разделил по месяцам

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -6,12 +6,21 @@ namespace Csharp
     {
         static void Main(string[] args)
         {
+            int[] allmonth = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
             Console.WriteLine("Введите колличество дней:");
             int day = Convert.ToInt32(Console.ReadLine());
             int year = day / 365;
-            int month = (day % 365) / 30;
-            day = (day % 365) % 30;
-            Console.WriteLine($"Летт = {year} \nМесяцев = {month} \nДней = {day}");
+            int i = 0;
+            int k = 0;
+            int month = 0;
+            day = day % 365;
+            while (day - allmonth[i] > 0)
+                    {
+                day = day - allmonth[i];
+                i++;
+                month++;
+                    }
+            Console.WriteLine($"Лет = {year} \nМесяцев = {month} \nДней = {day}");
             Console.ReadKey();
         }
     }
